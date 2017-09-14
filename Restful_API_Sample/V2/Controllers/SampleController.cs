42a42f61123b8f1dc5bee8a5f71f5ac1085736bf
ZhuLige <zhulige@qq.com>
2017-09-14 12:02:52 +0800

    V2

--- Restful_API_Sample/V2/Controllers/SampleController.cs
+++ Restful_API_Sample/V2/Controllers/SampleController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Restful_API_Sample.V2.Models;
 using System;
+using System.Collections.Generic;
 
 namespace Restful_API_Sample.V2.Controllers
 {
@@ -12,34 +13,107 @@ namespace Restful_API_Sample.V2.Controllers
     [Route("api/v{api-version:apiVersion}/[controller]")]
     public class SampleController : Controller
     {
+        const string ByIdRouteName = "GetById" + nameof(V2);
+
         /// <summary>
-        /// 用户登陆
+        /// 获取用户信息列表
         /// </summary>
-        /// <remarks>
-        /// Note that the key is a GUID and not an integer.
-        ///  
-        ///     POST /Login
-        ///     {
-        ///        "UserName": "用户名",
-        ///        "Password": "密码"
-        ///     }
-        /// 
-        /// </remarks>
-        /// <param name="login">Username:"用户名"<br/>Password:"密码"</param>
-        /// <returns>Token</returns>
-        /// <response code="200">返回Token</response>
-        /// <response code="400">用户名密码不能为空！</response>
-        [ProducesResponseType(typeof(Guid), 200)]
-        [ProducesResponseType(400)]
-        [HttpPost, Route("Login")]
-        public IActionResult Login([FromBody]Login login)
+        /// <returns>用户信息列表.</returns>
+        /// <response code="200">成功获取用户信息列表</response>
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<UserInfo>), 200)]
+        public IActionResult Get()
         {
-            if (login.UserName == null || login.Password == null)
+            var _UserName = new[]
             {
-                return BadRequest();
-            }
-            return Ok(Guid.NewGuid());
+                new UserInfo()
+                {
+                    Id = 1,
+                    UserName = "ZhuLige"
+                },
+                new UserInfo()
+                {
+                    Id = 2,
+                    UserName = "Bob"
+                },
+                new UserInfo()
+                {
+                    Id = 3,
+                    UserName = "Jane",
+                }
+            };
+
+            return Ok(_UserName);
         }
+
+        /// <summary>
+        /// 获取用户信息
+        /// </summary>
+        /// <param name="id">用户Id</param>
+        /// <returns>用户信息</returns>
+        /// <response code="200">成功获取用户信息</response>
+        /// <response code="400">未获取到用户信息</response>
+        [HttpGet("{id:int}", Name = ByIdRouteName)]
+        [ProducesResponseType(typeof(UserInfo), 200)]
+        [ProducesResponseType(400)]
+        public IActionResult Get(int id) =>
+            Ok(new UserInfo()
+            {
+                Id = id,
+                UserName = "ZhuLige"
+            }
+            );
+
+        /// <summary>
+        /// Creates a new person.
+        /// </summary>
+        /// <param name="userInfo">The person to create.</param>
+        /// <returns>The created person.</returns>
+        /// <response code="201">The person was successfully created.</response>
+        /// <response code="400">The person was invalid.</response>
+        [HttpPost]
+        [ProducesResponseType(typeof(UserInfo), 201)]
+        [ProducesResponseType(400)]
+        public IActionResult Post([FromBody] UserInfo userInfo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            userInfo.Id = 42;
+
+            return CreatedAtRoute(ByIdRouteName, new { id = userInfo.Id }, userInfo);
+        }
+
+        ///// <summary>
+        ///// 用户登陆
+        ///// </summary>
+        ///// <remarks>
+        ///// Note that the key is a GUID and not an integer.
+        /////  
+        /////     POST /Login
+        /////     {
+        /////        "UserName": "用户名",
+        /////        "Password": "密码"
+        /////     }
+        ///// 
+        ///// </remarks>
+        ///// <param name="login">Username:"用户名"<br/>Password:"密码"</param>
+        ///// <returns>Token</returns>
+        ///// <response code="200">返回Token</response>
+        ///// <response code="400">用户名密码不能为空！</response>
+        //[ProducesResponseType(typeof(Guid), 200)]
+        //[ProducesResponseType(400)]
+        //[HttpPost, Route("Login")]
+        //public IActionResult Login([FromBody]Login login)
+        //{
+        //    if (login.UserName == null || login.Password == null)
+        //    {
+        //        return BadRequest();
+        //    }
+        //    return Ok(Guid.NewGuid());
+        //}
 
         //// GET: api/Sample
         //[HttpGet]
---
+++ Restful_API_Sample/V2/Models/UserInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Restful_API_Sample.V2.Models
+{
+    /// <summary>
+    /// 用户信息
+    /// </summary>
+    public class UserInfo
+    {
+        /// <summary>
+        /// 用户Id
+        /// </summary>
+        /// <value>The person's unique identifier.</value>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        /// <value>The person's first name.</value>
+        [Required]
+        [StringLength(25)]
+        public string UserName { get; set; }
+        
+    }
+}
